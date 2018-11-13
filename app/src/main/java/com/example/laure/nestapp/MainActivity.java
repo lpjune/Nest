efce29c1c25ee990c3f43fa2a4aa55537b6c5a60
lpjune <laurenporsini@gmail.com>
2018-11-12 23:03:26 -0600

    fixed merge conflicts, top bar changes

--- app/src/main/java/com/example/laure/nestapp/MainActivity.java
+++ app/src/main/java/com/example/laure/nestapp/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button burgerButton, menuSettingsButton, menuConnectButton;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Switch.OnCheckedChangeListener{
+
+    // Button/Switch declaration
+    private Button burgerButton, menuSettingsButton, menuConnectButton, nextButton, backButton, systemHaltButton;
+    private Switch doorsSwitch, roofSwitch, extendPadSwitch, raisePadSwitch;
 
 
     @Override
@@ -16,19 +22,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         //start server connection
 
-        // button listeners
-//        int camID = 1;
-//        backButton = (Button)findViewById(R.id.backButton);
-//        nextButton = (Button)findViewById(R.id.nextButton);
+
+
+        // Button initializers
+        nextButton = (Button)findViewById(R.id.next_button);
+        backButton = (Button)findViewById(R.id.back_button);
         burgerButton = (Button)findViewById(R.id.burgerButton);
         menuConnectButton = (Button)findViewById(R.id.menuConnectBtn);
         menuSettingsButton = (Button)findViewById(R.id.menuSettingsBtn);
+        systemHaltButton = (Button)findViewById(R.id.systemHaltButton);
 
-        burgerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        // Switch initializers
+        doorsSwitch = (Switch)findViewById(R.id.doorsSwitch);
+        roofSwitch = (Switch)findViewById(R.id.roofSwitch);
+        extendPadSwitch = (Switch)findViewById(R.id.extendPadSwitch);
+        raisePadSwitch = (Switch)findViewById(R.id.raisePadSwitch);
+
+        // Button OnClickListeners
+        nextButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
+        burgerButton.setOnClickListener(this);
+        menuSettingsButton.setOnClickListener(this);
+        menuConnectButton.setOnClickListener(this);
+        systemHaltButton.setOnClickListener(this);
+
+        /// Switch OnClickListeners
+        doorsSwitch.setOnCheckedChangeListener(this);
+        roofSwitch.setOnCheckedChangeListener(this);
+        extendPadSwitch.setOnCheckedChangeListener(this);
+        raisePadSwitch.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String request = "";
+
+        // Change Request Code/ Request Parameters
+        switch (v.getId()){
+
+            case R.id.next_button:
+                request = "nextButton";
+                break;
+            case R.id.back_button:
+                request = "backButton";
+                break;
+            case R.id.burgerButton:
+
+                //Show or hide menu
                 if (menuConnectButton.getVisibility() == View.GONE){
                     menuConnectButton.setVisibility(View.VISIBLE);
                     menuSettingsButton.setVisibility(View.VISIBLE);
@@ -36,34 +80,51 @@ public class MainActivity extends AppCompatActivity {
                     menuConnectButton.setVisibility(View.GONE);
                     menuSettingsButton.setVisibility(View.GONE);
                 }
-            }
-        });
+                request = "burgerButton";
+                break;
+            case R.id.menuConnectBtn:
+                request = "menuConnectBtn";
+                break;
+            case R.id.menuSettingsBtn:
+                request = "menuSettingsBtn";
+                break;
+            case R.id.systemHaltButton:
+                request = "systemHaltButton";
+                break;
+              }
 
-//        backButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                if (camID == 1) {
-//                    return;
-//                }
-//                else {
-//                    camID -= 1;
-//                    return camID;
-//                }
-//            }
-//        });
+        //SEND REQUEST HERE
+        sendRequest(request);
+    }
 
-//        nextButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                if (camID == 2) {
-//                    return;
-//                }
-//                else {
-//                    camID += 1;
-//                    return camID;
-//                }
-//            }
-//        });
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String request = "";
+        String checked = Boolean.toString(isChecked);
 
+        // Change Request Code/ Request Parameters
+        switch(buttonView.getId()){
+            case R.id.doorsSwitch:
+                request = "doorsSwitch ";
+                break;
+            case R.id.roofSwitch:
+                request = "roofSwitch ";
+                break;
+            case R.id.extendPadSwitch:
+                request = "extendPadSwitch ";
+                break;
+            case R.id.raisePadSwitch:
+                request = "raisePadSwitch ";
+                break;
+        }
+
+        request += checked;
+
+        //SEND REQUEST HERE
+        sendRequest(request);
+    }
+
+    private void sendRequest(String requestCode){
+        Toast.makeText(this,requestCode,Toast.LENGTH_SHORT).show();
     }
 }
